bcb18f6592d42670ebe70dafe7b43585c1f64920
Linda <lihuiling987@gmail.com>
2020-01-06 22:26:21 +1300

    Forgot password

---
+++ EmployeeWebApplication/ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeWebApplication.ViewModels
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Token { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password",
+            ErrorMessage = "Password is not consist with the Confirmation password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
