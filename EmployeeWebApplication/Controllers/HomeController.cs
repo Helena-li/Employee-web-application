1868d861a6277fdb792feb2d716c19875214b7d7
Linda <lihuiling987@gmail.com>
2020-01-07 18:49:37 +1300

    Employee details

--- EmployeeWebApplication/Controllers/HomeController.cs
+++ EmployeeWebApplication/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EmployeeWebApplication.Models;
+using EmployeeWebApplication.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -13,21 +14,38 @@ namespace EmployeeWebApplication.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IEmployeeRepository employeeRepository;
+        private readonly IEmployeeRepository _employeeRepository;
         private readonly SignInManager<ApplicationUser> signInManager;
 
         public HomeController(IEmployeeRepository employeeRepository,
             SignInManager<ApplicationUser> signInManager)
         {
-            this.employeeRepository = employeeRepository;
+            this._employeeRepository = employeeRepository;
             this.signInManager = signInManager;
         }
         
         [AllowAnonymous]
         public IActionResult Index()
         {
-            var model = employeeRepository.GetEmployeeList();
+            var model = _employeeRepository.GetEmployeeList();
             return View(model);
+        }
+
+        public ViewResult Details(int id)
+        {
+            var employee = _employeeRepository.GetEmployee(id);
+            if (employee == null)
+            {
+                Response.StatusCode = 404;
+                return View("EmployeeNotFound", id);
+            }
+            HomeDetailViewModels homeDetailViewModels = new HomeDetailViewModels()
+            {
+                Employee = employee,
+                Title = "Emplyee Details"
+            };
+
+            return View(homeDetailViewModels);
         }
     }
 }
---
+++ EmployeeWebApplication/ViewModels/HomeDetailViewModels.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeWebApplication.ViewModels
+{
+    public class HomeDetailViewModels
+    {
+        public Employee Employee { get; set; }
+        public string Title { get; set; }
+    }
+}
